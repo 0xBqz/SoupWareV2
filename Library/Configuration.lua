732a3cc6070de5d25bdf8f20a684c903562c18a2
baba <97072588+0xBqz@users.noreply.github.com>
2023-02-12 10:50:40 -0800

    Update Configuration.lua

--- Library/Configuration.lua
+++ Library/Configuration.lua
@@ -1,17 +1,16 @@
 local HttpService = game:GetService("HttpService")
 local Place = tostring(game.PlaceId)
 
+local Library = {}
 local config = {}
 
 pcall(function()
-	config = HttpService:JSONDecode(readfile("SoupWareV2/"..Place..".json"))
+    config = HttpService:JSONDecode(readfile("SoupWareV2/"..Place..".json"))
 end)
 
 local function save()
-	writefile("SoupWareV2/"..Place..".json", HttpService:JSONEncode(config))
+    writefile("SoupWareV2/"..Place..".json", HttpService:JSONEncode(config))
 end
-
-local Library = {}
 
 function Library:Window(Title, Path)
 
@@ -25,7 +24,7 @@ function Library:Window(Title, Path)
 	Frame.Visible = false
 
 	local scroll = Instance.new("ScrollingFrame")
-	scroll.Name = "scroll"
+	scroll.Name = "confscroll"
 	scroll.Parent = Frame
 	scroll.Active = true
 	scroll.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -40,7 +39,7 @@ function Library:Window(Title, Path)
 	scroll.TopImage = "rbxasset://textures/ui/Scroll/scroll-middle.png"
 	scroll.ScrollBarImageColor3 = Color3.fromRGB(83, 83, 83)
 
-	--decorations
+	--top:
 
     local B_Frame = Instance.new("Frame")
     B_Frame.Name = "B_Frame"
@@ -48,10 +47,6 @@ function Library:Window(Title, Path)
     B_Frame.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
     B_Frame.Position = UDim2.new(-0.00175701198, 0, 0.00150996435, 0)
     B_Frame.Size = UDim2.new(0, 183, 0, 45)
-    
-    local UICorner_2 = Instance.new("UICorner")
-    UICorner_2.CornerRadius = UDim.new(0, 15)
-    UICorner_2.Parent = B_Frame
 
     local B_Frame_2 = Instance.new("Frame")
     B_Frame_2.Parent = B_Frame
@@ -71,11 +66,48 @@ function Library:Window(Title, Path)
     local B_Invis = Instance.new("Frame")
     B_Invis.Name = "invis"
     B_Invis.Parent = Frame
+    B_Invis.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     B_Invis.BackgroundTransparency = 1.000
     B_Invis.Size = UDim2.new(0, 179, 0, 45)
 
-	--top:
+    local Titles = Instance.new("TextLabel")
+    Titles.Name = "Title"
+    Titles.Parent = Frame
+    Titles.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Titles.BackgroundTransparency = 1.000
+    Titles.Position = UDim2.new(0.0546448082, 0, 0, 0)
+    Titles.Size = UDim2.new(0, 172, 0, 50)
+    Titles.Font = Enum.Font.SourceSansBold
+    Titles.Text = Title
+    Titles.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Titles.TextSize = 17.000
+    Titles.TextXAlignment = Enum.TextXAlignment.Left
 
+	--extras:
+
+    local UIListLayout = Instance.new("UIListLayout")
+    UIListLayout.Parent = B_Invis
+    UIListLayout.FillDirection = Enum.FillDirection.Horizontal
+    UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+    UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
+    UIListLayout.Padding = UDim.new(0, 5)
+
+    local UIListLayout_2 = Instance.new("UIListLayout")
+    UIListLayout_2.Parent = scroll
+    UIListLayout_2.HorizontalAlignment = Enum.HorizontalAlignment.Center
+    UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+    UIListLayout_2.Padding = UDim.new(0, 5)
+
+    local UICorner = Instance.new("UICorner")
+    UICorner.CornerRadius = UDim.new(0, 15)
+    UICorner.Parent = Frame
+
+    local UICorner_2 = Instance.new("UICorner")
+    UICorner_2.CornerRadius = UDim.new(0, 15)
+    UICorner_2.Parent = B_Frame
+
+	--close
+	
     local close = Instance.new("ImageButton")
     close.Name = "close"
     close.Parent = B_Invis
@@ -94,45 +126,13 @@ function Library:Window(Title, Path)
         Frame.Visible = false
     end)
 
-    local Titles = Instance.new("TextLabel")
-    Titles.Name = "Title"
-    Titles.Parent = Frame
-    Titles.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Titles.BackgroundTransparency = 1.000
-    Titles.Position = UDim2.new(0.0546448082, 0, 0, 0)
-    Titles.Size = UDim2.new(0, 172, 0, 50)
-    Titles.Font = Enum.Font.SourceSansBold
-    Titles.Text = Title
-    Titles.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Titles.TextSize = 17.000
-    Titles.TextXAlignment = Enum.TextXAlignment.Left
-
-	--extras:
-
-    local UICorner = Instance.new("UICorner")
-    UICorner.CornerRadius = UDim.new(0, 15)
-    UICorner.Parent = Frame
-
-    local UIListLayout = Instance.new("UIListLayout")
-    UIListLayout.Parent = B_Frame
-    UIListLayout.FillDirection = Enum.FillDirection.Horizontal
-    UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-    UIListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
-    UIListLayout.Padding = UDim.new(0, 5)
-
-    local UIListLayout_2 = Instance.new("UIListLayout")
-    UIListLayout_2.Parent = scroll
-    UIListLayout_2.HorizontalAlignment = Enum.HorizontalAlignment.Center
-    UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
-    UIListLayout_2.Padding = UDim.new(0, 5)
-
-    local function resize()
-		pcall(function()
-			scroll.CanvasSize = UDim2.new(0, 0, 0, UIListLayout_2.AbsoluteContentSize.Y)
-		end)
-    end
+	--start
 
 	local UI = {}
+
+	local function resize()
+		scroll.CanvasSize = UDim2.new(0, 0, 0, UIListLayout_2.AbsoluteContentSize.Y)
+	end
 
     --text:
 
@@ -149,7 +149,7 @@ function Library:Window(Title, Path)
         text.TextColor3 = Color3.fromRGB(255, 255, 255)
         text.TextSize = 14.000
 
-        resize()
+		resize()
     end
 
     --button
@@ -178,6 +178,7 @@ function Library:Window(Title, Path)
         Title.BackgroundTransparency = 1.000
         Title.Position = UDim2.new(0.0740740746, 0, 0, 0)
         Title.Size = UDim2.new(0, 79, 0, 34)
+        Title.Text = tostring(settings.Name)
         Title.Font = Enum.Font.SourceSansBold
         Title.TextColor3 = Color3.fromRGB(255, 255, 255)
         Title.TextSize = 17.000
@@ -202,7 +203,7 @@ function Library:Window(Title, Path)
 		local UICorner_2 = Instance.new("UICorner")
         UICorner_2.Parent = Frame
 
-        Title.Text = tostring(settings.Name)
+		local settings = {}
 
 		function settings:execute()
 			pcall(settings.Callback)
@@ -212,7 +213,7 @@ function Library:Window(Title, Path)
             settings:execute()
         end)
 
-        resize()
+		resize()
 
 		return settings
     end
@@ -231,11 +232,17 @@ function Library:Window(Title, Path)
         toggle.TextColor3 = Color3.fromRGB(0, 0, 0)
         toggle.TextSize = 14.000
 
-        local Frame = Instance.new("Frame")
-        Frame.Parent = toggle
-        Frame.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
-        Frame.Position = UDim2.new(0.0149999997, 0, 0.0780000016, 0)
-        Frame.Size = UDim2.new(0, 158, 0, 32)
+        local Container = Instance.new("Frame")
+        Container.Parent = toggle
+        Container.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
+        Container.Position = UDim2.new(0.0149999997, 0, 0.0780000016, 0)
+        Container.Size = UDim2.new(0, 158, 0, 32)
+
+        local Bar = Instance.new("Frame")
+        Bar.Parent = Container
+        Bar.BackgroundColor3 = Color3.fromRGB(180, 180, 180)
+        Bar.Position = UDim2.new(0.727848113, 0, 0.392700195, 0)
+        Bar.Size = UDim2.new(0, 34, 0, 5)
 
         local Title = Instance.new("TextLabel")
         Title.Parent = toggle
@@ -251,20 +258,16 @@ function Library:Window(Title, Path)
 
         local Dot = Instance.new("Frame")
         Dot.Parent = toggle
+        Dot.BackgroundColor3 = Color3.fromRGB(139, 139, 139)
+        Dot.Position = UDim2.new(0.701675158, 0, 0.257565081, 0)
         Dot.Size = UDim2.new(0, 15, 0, 15)
-
-        local Bar = Instance.new("Frame")
-        Bar.Parent = Frame
-        Bar.BackgroundColor3 = Color3.fromRGB(180, 180, 180)
-        Bar.Position = UDim2.new(0.727848113, 0, 0.392700195, 0)
-        Bar.Size = UDim2.new(0, 34, 0, 5)
 
         local UICorner = Instance.new("UICorner")
         UICorner.CornerRadius = UDim.new(0, 5)
         UICorner.Parent = toggle
 
         local UICorner_2 = Instance.new("UICorner")
-        UICorner_2.Parent = Frame
+        UICorner_2.Parent = Container
 
         local UICorner_3 = Instance.new("UICorner")
         UICorner_3.CornerRadius = UDim.new(0, 15)
@@ -275,15 +278,12 @@ function Library:Window(Title, Path)
         UICorner_4.Parent = Dot
 
 		local value = config[Path..settings.Name] and true or settings.Default or false
-
+ 
         if value then
 			pcall(settings.Callback, true)
 
             Dot.BackgroundColor3 = Color3.fromRGB(80, 153, 195)
-            Dot.Position = UDim2.new(0.845131934, 0, 0.257565081, 0)
-        else
-            Dot.BackgroundColor3 = Color3.fromRGB(139, 139, 139)
-            Dot.Position = UDim2.new(0.701675158, 0, 0.257565081, 0)
+			Dot.Position = UDim2.new(0.845131934, 0, 0.257565081, 0)
         end
 
 		function settings:getvalue()
@@ -296,12 +296,12 @@ function Library:Window(Title, Path)
 			save()
 
 			if value then
-                game:GetService("TweenService"):Create(Dot, TweenInfo.new(0.8), {
+                game:GetService("TweenService"):Create(Dot,TweenInfo.new(0.8),{
 					BackgroundColor3 = Color3.fromRGB(80, 153, 195),
                     Position = UDim2.new(0.845131934, 0, 0.257565081, 0)
 				}):Play()
 			else
-                game:GetService("TweenService"):Create(Dot, TweenInfo.new(0.8), {
+                game:GetService("TweenService"):Create(Dot,TweenInfo.new(0.8),{
 					BackgroundColor3 = Color3.fromRGB(139, 139, 139),
                     Position = UDim2.new(0.701675158, 0, 0.257565081, 0)
 				}):Play()
@@ -315,11 +315,15 @@ function Library:Window(Title, Path)
 			settings:changevalue(value)
 		end
 
+		function settings:get()
+			return value
+		end
+
         toggle.MouseButton1Click:Connect(function()
 			settings:toggle()
         end)
 
-        resize()
+		resize()
 
 		return settings
     end
@@ -338,11 +342,11 @@ function Library:Window(Title, Path)
         keybind.TextColor3 = Color3.fromRGB(0, 0, 0)
         keybind.TextSize = 14.000
 
-        local Frame = Instance.new("Frame")
-        Frame.Parent = keybind
-        Frame.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
-        Frame.Position = UDim2.new(0.0149999997, 0, 0.0780000016, 0)
-        Frame.Size = UDim2.new(0, 158, 0, 32)
+        local Container = Instance.new("Frame")
+        Container.Parent = keybind
+        Container.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
+        Container.Position = UDim2.new(0.0149999997, 0, 0.0780000016, 0)
+        Container.Size = UDim2.new(0, 158, 0, 32)
 
         local Title = Instance.new("TextLabel")
         Title.Parent = keybind
@@ -356,36 +360,36 @@ function Library:Window(Title, Path)
         Title.TextSize = 17.000
         Title.TextXAlignment = Enum.TextXAlignment.Left
 
-        local Label = Instance.new("TextLabel")
-        Label.Parent = keybind
-        Label.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-        Label.Position = UDim2.new(0.55, 0, 0.175675675, 0)
-        Label.Size = UDim2.new(0, 56, 0, 20)
-        Label.Font = Enum.Font.SourceSansBold
-        Label.TextColor3 = Color3.fromRGB(255, 255, 255)
-        Label.TextSize = 16.000
+        local Input = Instance.new("TextLabel")
+        Input.Parent = keybind
+        Input.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+		Input.Position = UDim2.new(0.55, 0, 0.175675675, 0)
+        Input.Size = UDim2.new(0, 56, 0, 20)
+        Input.Font = Enum.Font.SourceSansBold
+        Input.TextColor3 = Color3.fromRGB(255, 255, 255)
+        Input.TextSize = 16.000
 
         local UICorner = Instance.new("UICorner")
         UICorner.CornerRadius = UDim.new(0, 5)
         UICorner.Parent = keybind
 
         local UICorner_2 = Instance.new("UICorner")
-        UICorner_2.Parent = Frame
+        UICorner_2.Parent = Container
 
         local UICorner_3 = Instance.new("UICorner")
         UICorner_3.CornerRadius = UDim.new(0, 10)
-        UICorner_3.Parent = Label
+        UICorner_3.Parent = Input
 
 		local ShowKey = config[Path..settings.Name] and Enum.KeyCode[config[Path..settings.Name]].Name or settings.Default and Enum.KeyCode[settings.Default] or settings.Default or nil
-		Label.Text = ShowKey and ShowKey.Name or ". . .";
+		Input.Text = ShowKey and ShowKey or ". . .";
 
 		local Key = ShowKey
 
 		keybind.MouseButton1Click:Connect(function()
-			Label.Text = ". . ."
+			Input.Text = ". . ."
 			local inputwait = game:GetService("UserInputService").InputBegan:wait()
 			if inputwait.KeyCode.Name ~= "Unknown" then
-				Label.Text = inputwait.KeyCode.Name
+				Input.Text = inputwait.KeyCode.Name
 				Key = inputwait.KeyCode.Name
 
 				config[Path..settings.Name] = inputwait.KeyCode.Name
@@ -402,10 +406,10 @@ function Library:Window(Title, Path)
 			end
 		end)
 
-        resize()
-    end
+		resize()
+	end
 
-    --getframe
+    --get frame
 
     function UI:getframe()
         return Frame
